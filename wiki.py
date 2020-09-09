c37dc00be218d3cd3f270618c31aac0c6b740bca
Maia <maia.mcc@gmail.com>
2020-09-08 20:43:22 -0400

    working pageview check

--- wiki.py
+++ wiki.py
@@ -1,29 +1,42 @@
-# import requests
-import json
+#! /usr/bin/env python
+from datetime import date
+import sys
+
+import requests
 
 ITEMS = 'items'
 VIEWS = 'views'
-# https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/user/Albert_Einstein/monthly/2020010100/3030090100
+
+FIRST_OF_THIS_MONTH = date.today().strftime("%Y%m0100")
+
+def query_url(name: str) -> str:
+    # TODO: verify the name is right
+    return 'https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/user/{name}/monthly/2018010100/{end_date}'.format(name=name.replace(' ', '_'), end_date=FIRST_OF_THIS_MONTH)
+
 
 # POPULARITY CHECK:
 # for every name, search on wiki. If page, get views for last x months and calculate views per month (or total views?)
 # if no page, will check manually
 # order by page views and see if we care
+def views_per_month(name: str) -> int:
+    url = query_url(name)
+    r = requests.get(url)
+    if r.status_code != 200:
+        j = r.json()
+        raise Exception('Request failed with status {} ({}). Message:\n\t{}'.
+                        format(r.status_code, j.get('title', '<no title>'), j.get('detail', '<no detail>')))
+    payload = r.json()
+    items = payload[ITEMS]
+    total_views = 0
+    num_months = 0
+    for item in items:
+        num_months += 1
+        total_views += int(item[VIEWS])
 
-# Dan Castellaneta
-# Kevin Dillon
+    print('total views: {}'.format(total_views))
+    print('views per month: {}'.format(total_views/num_months))
 
-raw = '''{"items":[{"project":"en.wikipedia","article":"Dan_Castellaneta","granularity":"monthly","timestamp":"2020010100","access":"all-access","agent":"user","views":55489},{"project":"en.wikipedia","article":"Dan_Castellaneta","granularity":"monthly","timestamp":"2020020100","access":"all-access","agent":"user","views":44984},{"project":"en.wikipedia","article":"Dan_Castellaneta","granularity":"monthly","timestamp":"2020030100","access":"all-access","agent":"user","views":51493},{"project":"en.wikipedia","article":"Dan_Castellaneta","granularity":"monthly","timestamp":"2020040100","access":"all-access","agent":"user","views":66431},{"project":"en.wikipedia","article":"Dan_Castellaneta","granularity":"monthly","timestamp":"2020050100","access":"all-access","agent":"user","views":66785},{"project":"en.wikipedia","article":"Dan_Castellaneta","granularity":"monthly","timestamp":"2020060100","access":"all-access","agent":"user","views":57096},{"project":"en.wikipedia","article":"Dan_Castellaneta","granularity":"monthly","timestamp":"2020070100","access":"all-access","agent":"user","views":49455},{"project":"en.wikipedia","article":"Dan_Castellaneta","granularity":"monthly","timestamp":"2020080100","access":"all-access","agent":"user","views":65651},{"project":"en.wikipedia","article":"Dan_Castellaneta","granularity":"monthly","timestamp":"2020090100","access":"all-access","agent":"user","views":10907}]}'''
+    return total_views/num_months
 
-payload = json.loads(raw)
-
-items = payload[ITEMS]
-total_views = 0
-num_months = 0
-for item in items:
-    num_months += 1
-    total_views += int(item[VIEWS])
-
-print('DAN CAST...')
-print('total views: {}'.format(total_views))
-print('views per month: {}'.format(total_views/num_months))
+if __name__ == '__main__':
+    views_per_month(sys.argv[1])
